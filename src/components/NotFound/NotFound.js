951a48912273db1c08d2fdaae7a51ec8e8c97f1a
unknown <rajebul.alam17@gmail.com>
2022-04-04 23:26:34 +0600

    Not Found Part Commited

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404</h2>
+        <div className='notFound-container'>
+            <h1>Opps!</h1>
+            <h2>404 - PAGE NOT FOUND</h2>
+            <p>The page you are looking for might have been removed</p>
+            <p>had its name changed or is temporarily unavailable</p>
+            <Link to="/">GO TO HOME PAGE</Link>
         </div>
     );
 };
