527f1cf522e4c9a6e4f26b85d3ef0780eca1ea5e
unknown <rajebul.alam17@gmail.com>
2022-04-04 22:50:45 +0600

    Header Part Commited

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header Part</h2>
+        <div className='header-container'>
+            <div className='logo-part'>
+                <Link to='/'>KHOKAN</Link>
+            </div>
+            <div className='menu-bar'>
+                <nav>
+                    <Link to = '/'>HOME</Link>
+                    <Link to = '/reviews'>REVIEWS</Link>
+                    <Link to = '/dashboard'>DASHBOARD</Link>
+                    <Link to = '/blogs'>BLOGS</Link>
+                    <Link to = '/about'>ABOUT</Link>
+                </nav>
+            </div>
         </div>
     );
 };
