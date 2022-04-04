412a41268fd4dc7fd77ade528c96f775a1193573
unknown <rajebul.alam17@gmail.com>
2022-04-04 23:22:22 +0600

    About Part Commited

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './About.css';
+import personal from '../../images/shape-4.png';
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className='about-container'>
+            <h2>About Me</h2>
+            <div>
+                <img src={personal} alt="personal-img" />
+                <p> I'm <strong>Rajebul Alam Khokan</strong>. I'm a professional web developer. I'm capable to learn and I'm also hard worker. I'm confident and believe about my self.</p>
+            </div>
         </div>
     );
 };
