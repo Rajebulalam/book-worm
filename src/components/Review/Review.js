642eaa1b7c277b7423483c053e1560ff4300638e
unknown <rajebul.alam17@gmail.com>
2022-04-04 23:02:36 +0600

    Review Part Commited

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Review.css';
 
-const Review = () => {
+const Review = ({ reviewItem }) => {
+
+    const { img, name, review, ratings } = reviewItem;
+
     return (
-        <div>
-            <h2>Review</h2>
+        <div className='review-item'>
+            <img src={img} alt="review-person" />
+            <h2> Name : {name} </h2>
+            <p> Review : {review} </p>
+            <h3> Ratings : {ratings} start </h3>
         </div>
     );
 };
